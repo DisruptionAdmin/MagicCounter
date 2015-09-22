bfca6f2355a80595ae6bf865aecd6c33deacd8e6
xbaddy <willc1991@gmail.com>
2015-09-21 21:50:01 -0400

    First Commit

---
+++ app/src/main/java/com/badfish/magiccounter/Player.java
@@ -0,0 +1,15 @@
+package com.badfish.magiccounter;
+
+/**
+ * Created by Will on 9/17/2015.
+ */
+public class Player
+{
+    //create button and text field for each player
+    public Player()
+    {
+
+    }
+
+
+}
